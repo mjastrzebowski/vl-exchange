f4d000d8b26abdf4b009d54af0d07465f88f8fc0
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2018-05-21 13:17:01 +0200

    More Form tests

--- test/unit/form.spec.ts
+++ test/unit/form.spec.ts
@@ -1,5 +1,7 @@
 import './setup';
 import { HttpClient } from 'aurelia-fetch-client';
+
+import { Currency } from '../../src/converter';
 import { Form } from '../../src/form';
 
 class HttpStub extends HttpClient {
@@ -19,28 +21,60 @@ class HttpStub extends HttpClient {
   }
 }
 
-describe('the Form module', () => {
-  it('amountTo is computed correctly', () => {
-    var amountFrom = 2;
-    var rateNumber = 3;
-    var result = 6;
+describe('Form module', () => {
+  describe('amountTo property', () => {
+    it('is computed correctly', () => {
+      var amountFrom = 2;
+      var rateNumber = 3;
+      var result = 6;
 
-    var form = new Form();
-    form.amountFrom = amountFrom;
-    form.rateNumber = rateNumber;
-    
-    expect(form.amountTo).toBe(result);
+      var form = new Form();
+      form.amountFrom = amountFrom;
+      form.rateNumber = rateNumber;
+      
+      expect(form.amountTo).toBe(result);
+    });
+
+    it('cannot be set manually', () => {
+      var amountTo = 2;
+
+      var form = new Form();
+      try {
+        form.amountTo = amountTo;
+      } catch (e) {
+        expect(e).not.toBeNull();
+      }
+      expect(form.amountTo).not.toBe(amountTo);
+    });
   });
 
-  it('amountTo cannot be set manually', () => {
-    var amountTo = 2;
+  describe('swap() function', () => {
+    it('currency is swaped', () => {
+      var currencyFrom = Currency.PLN;
+      var currencyTo = Currency.EUR;
 
-    var form = new Form();
-    try {
-      form.amountTo = amountTo;
-    } catch (e) {
-      expect(e).not.toBeNull();
-    }
-    expect(form.amountTo).not.toBe(amountTo);
+      var form = new Form();
+      form.currencyFrom = currencyFrom;
+      form.currencyTo = currencyTo;
+      form.swap();
+
+      expect(form.currencyFrom).toBe(currencyTo);
+      expect(form.currencyTo).toBe(currencyFrom);
+    });
+
+    it('amount is swaped', () => {
+      var amountFrom = 2;
+      var rateNumber = 3;
+      var amountTo = 6;
+      var expected = amountTo * rateNumber;
+
+      var form = new Form();
+      form.amountFrom = amountFrom;
+      form.rateNumber = rateNumber;
+
+      form.swap();
+
+      expect(form.amountTo).toBe(expected);
+    });
   });
 });
